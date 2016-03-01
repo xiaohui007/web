bb34975e9f93136937def1fc1b3404d782165797
xiaohui007 <mmjhui@qq.com>
2016-03-01 16:03:01 +0800

    车贷
    修改

--- financial_services-shuoming.php
+++ financial_services-shuoming.php
@@ -1,10 +1,14 @@
-<?php $page_title="车贷指南-金融服务"; $page_class="金融服务"; $page_name="车贷指南"; include 'inc/header.php'; ?>
+<?php 
+$page_title="车贷指南-金融服务"; 
+$page_class="金融服务"; 
+$page_name="车贷指南"; 
+include 'inc/header.php'; ?>
 
 <!-- start content -->
 <?php include 'inc/banner.php'; ?>
 <?php include 'inc/menu_sec.php'; ?>
 
-
+sdfafsdf
 
 
 <!-- end content -->
