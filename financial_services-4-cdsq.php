d7341232f7ae1a52de27d8caa9a6c2e80092cf47
xiaohui007 <mmjhui@qq.com>
2016-03-02 15:03:54 +0800

    车贷申请

---
+++ financial_services-4-cdsq.php
@@ -0,0 +1,12 @@
+<?php $page_title="车贷申请-金融服务"; $page_class="金融服务"; $page_name="车贷申请"; include 'inc/header.php'; ?>
+
+<!-- start content -->
+<?php include 'inc/banner.php'; ?>
+<?php include 'inc/menu_sec.php'; ?>
+
+<div class="w1200">
+<img src="images/financial/cdsq.jpg" />
+</div>
+<!-- end content -->
+
+<?php include 'inc/footer.php'; ?>
