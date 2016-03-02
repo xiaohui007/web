aff6aa38fd60755114fed52bd172f511a3b368e9
xiaohui007 <mmjhui@qq.com>
2016-03-02 15:03:30 +0800

    车贷计算

---
+++ financial_services-2-cdjs.php
@@ -0,0 +1,12 @@
+<?php $page_title="车贷计算-金融服务"; $page_class="金融服务"; $page_name="车贷计算"; include 'inc/header.php'; ?>
+
+<!-- start content -->
+<?php include 'inc/banner.php'; ?>
+<?php include 'inc/menu_sec.php'; ?>
+
+<div class="w1200">
+<img src="images/financial/cdjs.jpg" />
+</div>
+<!-- end content -->
+
+<?php include 'inc/footer.php'; ?>
