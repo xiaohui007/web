55800fdb6c2903b1418c4c08b09bc84da2259475
xiaohui007 <mmjhui@qq.com>
2016-03-02 15:04:04 +0800

    常见问题

---
+++ financial_services-5-cjwt.php
@@ -0,0 +1,13 @@
+<?php $page_title="常见问题-金融服务"; $page_class="金融服务"; $page_name="常见问题"; include 'inc/header.php'; ?>
+
+<!-- start content -->
+<?php include 'inc/banner.php'; ?>
+<?php include 'inc/menu_sec.php'; ?>
+
+<div class="w1200">
+<img src="images/financial/cjwt1.jpg" />
+<img src="images/financial/cjwt2.jpg" />
+</div>
+<!-- end content -->
+
+<?php include 'inc/footer.php'; ?>
