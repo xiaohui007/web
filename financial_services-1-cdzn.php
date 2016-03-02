c49680334239d2b53e62ecc8611d78969863090d
xiaohui007 <mmjhui@qq.com>
2016-03-02 15:03:13 +0800

    车贷指南

---
+++ financial_services-1-cdzn.php
@@ -0,0 +1,15 @@
+<?php $page_title="车贷指南-金融服务"; $page_class="金融服务"; $page_name="车贷指南"; include 'inc/header.php'; ?>
+
+<!-- start content -->
+<?php include 'inc/banner.php'; ?>
+<?php include 'inc/menu_sec.php'; ?>
+
+<div class="w1200">
+<img src="images/financial/cdzn1.jpg" />
+<img src="images/financial/cdzn2.jpg" />
+<img src="images/financial/cdzn3.jpg" />
+<img src="images/financial/cdzn4.jpg" />
+</div>
+<!-- end content -->
+
+<?php include 'inc/footer.php'; ?>
